d9961702c3e9d66ffca12e6b6f132f41ccc2c23f
Osama Mussawir <omussawir@nisum.com>
2019-11-14 00:21:46 +0500

    master: insert script changed

--- target/classes/db/migration/V1.2__insert_data.sql
+++ target/classes/db/migration/V1.2__insert_data.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS PERSONA_INFO (
+  id INT  NOT NULL  ,
+  address VARCHAR (100),
+  hair_colour VARCHAR(10)  ,
+  last_name VARCHAR(10)  ,
+  name VARCHAR(10)  ,
+  simple_phone_number INT
+);
+
 insert into persona_info (id , address , hair_colour , last_name , name , simple_phone_number) values (10001 , ' gulberg ' , 'black' , ' osama1' , ' osama' , 2131231 );
 
 insert into persona_info (id , address , hair_colour , last_name , name , simple_phone_number) values (10002 , ' gulberg ' , 'black' , ' osama2' , ' osama' , 2131231 );
